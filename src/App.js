7659f8dee31cd51409788b5a54e39dac76d9eaeb
Alison Hedigliranes <alison.hedigliranes@gmail.com>
2023-05-10 17:10:40 -0300

    tela inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+          <div className='logo'>
+           <h1>
+              <span style={{color:"#0069CA"}}>Search </span>
+              <span style={{color:"#8C19D2"}}>d_evs</span>
+            </h1>
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+            <input type="text" class="col-md-10 col-sm-10 col-10 search form-control" placeholder="Insira o Username do Usuário GitHub" aria-label="Username" aria-describedby="basic-addon1"></input>
+            <button className="col-md-2 col-sm-2 col-2 btn" type="submit">Buscar</button>
+        </div>
+      </div>
     </div>
   );
 }
